8f8ddc8a9b6264c629d15564a9e9fe9202f5e317
smolito <smolda.tom@gmail.com>
2025-12-01 22:26:08 +0100

    english terminal output

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "${GREEN}[1/3] Kontrola dat...${NC}"
+echo -e "${GREEN}[1/3] Checking available data...${NC}"
 
 # data URL
 DATA_URL="https://bigdataexercise01.blob.core.windows.net/bigdata-exercises-container/merged_samples.vcf.gz"
@@ -16,23 +16,23 @@ mkdir -p $DATA_DIR
 
 # check for file
 if [ -f "$DATA_DIR/$FILE_NAME" ]; then
-    echo "Soubor $FILE_NAME již existuje. Přeskakuji stahování."
+    echo "File $FILE_NAME already exists. Skipping the download."
 else
-    echo "Stahuji data z $DATA_URL..."
+    echo "Downloading data from $DATA_URL..."
     # use either curl or wget
     if command -v curl &> /dev/null; then
         curl -L -o "$DATA_DIR/$FILE_NAME" "$DATA_URL"
     elif command -v wget &> /dev/null; then
         wget -O "$DATA_DIR/$FILE_NAME" "$DATA_URL"
     else
-        echo "Chyba: Nemám curl ani wget. Stáhni data ručně."
+        echo "Error: Neither curl nor wget is available. Download manually."
         exit 1
     fi
 fi
 
-echo -e "${GREEN}[2/3] Spouštění databáze...${NC}"
+echo -e "${GREEN}[2/3] Launching the database from docker-compose...${NC}"
 # run docker-compose
 docker-compose up -d
 
 echo -e "${GREEN}[3/3] Hotovo!${NC}"
-echo "Databáze běží. Nyní můžeš spustit ETL proces."
+echo "Database is running. Ready for ETL pipeline."
